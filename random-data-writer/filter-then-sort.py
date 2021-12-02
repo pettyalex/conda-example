e833ec42f94c637568d49bc64589320a68cba994
Alex Petty <pettyalex@gmail.com>
2021-12-02 11:04:09 -0600

    Add a directory demonstrating profiling. Uses snakeviz to visualize output, and a notebook to generate random data

---
+++ random-data-writer/filter-then-sort.py
@@ -0,0 +1,26 @@
+from functions import is_small
+
+def read_in_data():
+    with open('random-data.txt') as file:
+        lines = []
+        
+        for line in file:
+            lines.append(int(line.strip()))
+    
+    return lines
+
+def sort(values: list):
+    values.sort()
+    return values
+
+def do_filter(values: list):
+    filtered_items = []
+    for i in filter(is_small, values):
+        filtered_items.append(i)
+    return filtered_items
+
+numbers = read_in_data()
+filtered = do_filter(numbers)
+sorted = sort(filtered)
+
+print(f"There are {len(sorted)} numbers")
